ff9c6360c305c505b5924c88aceb70f1a6a6e0b0
Anuragkushwah21 <anuragkushwah5100@gmail.com>
2024-08-12 17:38:49 +0530

    application co

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -4,6 +4,7 @@ function Application() {
   return (
     <div>
       <h1>Application</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic reprehenderit officia cupiditate esse blanditiis distinctio aliquid deserunt, ut vel molestiae.</p>
     </div>
   )
 }
